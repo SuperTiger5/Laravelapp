040916a24ad38b5d75b988d133e72f4ba6dedf63
st <thankyouberry5.8@gmail.com>
2022-01-17 03:32:52 +0900

    r

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,24 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function __invoke() {
-        return <<<EOF
-        <html>
-        <head>
-        <title>Hello</title>
-        <style>
-        body {font-size:16pt; color:#999;}
-        h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
-        </style>
-        </head>
-        <body>
-        <h1>Single Action</h1>
-        <p>これは、シングルアクションコントローラのindexアクションです。</p>
-        </body>
-        </html>
-        EOF;
+    public function index() {
+        $data = [
+            'msg' => 'お名前を入力して下さい。'
+        ];
+        return view('hello.index', $data);
+    }
+
+    public function post(Request $request) {
+        $msg = $request->msg;
+        $data = [
+            'msg' => 'こんにちは、'. $msg. 'さん!'
+        ];
+        return view('hello.index', $data);
     }
 }
