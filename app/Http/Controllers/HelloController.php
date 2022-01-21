79cb8e80a2bf6f581dca2072356e4b0c1c8c989b
st <thankyouberry5.8@gmail.com>
2022-01-21 17:56:15 +0900

    バリデーションの基本処理finished

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request) {//①
         return view('hello.index', ['msg'=>'フォームを入力：']);
     }
 
@@ -18,7 +18,7 @@ class HelloController extends Controller
             'mail' => 'email',
             'age' => 'numeric|between:0,150'
         ];
-        $this->validate($request, $validate_rule);
+        $this->validate($request, $validate_rule);//ここでエラーが発生すれば①へ飛ぶ
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
